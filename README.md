a017537c639193a9cb450f81e1f53046eb53c8d3
warsan <warsan00@mail.ru>
2021-04-24 10:36:41 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-start-proekt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-start-proekt)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <HelloWorld msg="Добро пожаловать в ваше приложение Vue.js "/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
